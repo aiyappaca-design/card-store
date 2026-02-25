6af71dc6b50f00f9aaf5ff7238be87976b26987e
Aiyappa C A <aiyappaca@gmail.com>
2026-02-25 14:14:54 +0530

    Second Commit

--- database.py
+++ database.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "your_supabase_connection_string"
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
